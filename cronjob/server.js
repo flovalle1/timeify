9a20a725574cb41bae08b6d0f208a7412eb6f04e
Florian Martel <hello@florianmartel.de>
2025-01-27 11:43:13 +0100

    feat: introduce cronjob server for reminders

---
+++ cronjob/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const schedule = require('node-schedule');
+const { Resend } = require('resend');
+require('dotenv').config({ path: '../.env' });
+
+const domain = process.env.DOMAIN;
+const resend_api_key = process.env.RESEND_API_KEY;
+const personal_email = process.env.PERSONAL_EMAIL;
+const resend = new Resend(resend_api_key);
+const app = express();
+
+var tracking_started = false;
+
+app.use(express.json());
+
+schedule.scheduleJob('30 9 * * *', () => {
+    if (!tracking_started) {
+        resend.emails.send({
+            from: `Timify Reminder <timify@${domain}>`,
+            to: [personal_email],
+            subject: 'Track Time now!',
+            html: '<p>You havent started your time tracking. Do it now by starting Timify on your console!</p>',
+        });
+    }
+});
+
+schedule.scheduleJob('59 23 * * *', () => {
+    tracking_started = false;
+});
+
+app.post('/tracking-started', (req, res) => {
+    tracking_started = true;
+});
+
+app.listen(3000, () => console.log('Cronjob Server running on port 3000'));
